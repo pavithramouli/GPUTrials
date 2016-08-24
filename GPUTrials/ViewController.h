b5d2c6650a8785b08893cc5f6f9c8a44677cf612
GTL <pavithra.sapna@gmail.com>
2016-08-24 13:33:38 +0530

    Initial Commit

---
+++ GPUTrials/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GPUTrials
+//
+//  Created by Ideas2IT-Pavithramouli on 24/08/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GPUTrials/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GPUTrials
+//
+//  Created by Ideas2IT-Pavithramouli on 24/08/16.
+//  Copyright © 2016 Personal. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
